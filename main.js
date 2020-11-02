80b8e5acc790c78bd99664fc793e2dfeb55e4c0a
Ekansh-1 <73730956+Ekansh-1@users.noreply.github.com>
2020-11-02 13:15:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+canvas=document.getElementById("myCanvas");
+
+ctx=canvas.getContext("2d");
+
+rover_width=100;
+rover_height=90;
+
+background_img="mars.jpg";
+
+rover_img="rover.png";
+rover_X=10;
+rover_Y=10;
